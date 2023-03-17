b4583068ee84d6a7a7a3427ba3bbef49c84245a9
staff <ticher@macbook-pro-maksym.home>
2023-03-17 19:56:46 +0200

    first commit

---
+++ exemple 2/index.js
@@ -0,0 +1,13 @@
+alert('Test message')
+const b = confirm('Confirm action')
+console.log('b', b)
+const c = prompt('Add text')
+console.log('c', c)
+// confirm are you here
+confirm('Are you here?')
+
+/*
+*
+*
+* */
+
